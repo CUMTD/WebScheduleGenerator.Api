dfbed257fc91853d0c144cb27d60c73d725a5a94
Ryan Blackman <rblackman@mtd.org>
2024-01-04 16:57:55 -0600

    fix ef and add parens

--- Core/Entities/Schedule/Stop.cs
+++ Core/Entities/Schedule/Stop.cs
@@ -4,6 +4,7 @@ namespace WebScheduleGenerator.Core.Entities.Schedule
 	{
 		public required string Id { get; set; }
 		public required string Name { get; set; }
+		public string? Parentheses { get; set; }
 		public double Latitude { get; set; }
 		public double Longitude { get; set; }
 		public required string SmsCode { get; set; }
